aef08c13ae36c7f14ff7a25a354efe4422afd592
vikGH <vikkiwei@yahoo.com>
2023-02-12 23:32:10 -0800

    Update main.tf
    auto scaling

--- main.tf
+++ main.tf
@@ -31,18 +31,37 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+//resource "aws_instance" "blog" {
+//  ami                    = data.aws_ami.app_ami.id
+//  instance_type          = var.instance_type
+//  subnet_id              = module.blog_vpc.public_subnets[0]
+//  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+//  tags = {
+//    Name = "Learning Terraform"
+//  }
+//}
+
+    module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.1"
+      
+      name = "blog"
+      min_size = 1
+      max_size = 2
+      
+      vpc_zone_identifier = module.blog_vpc.public_subnets
+      target_group_arns   = module.blog_alb.target_group_arns
+      security_groups = [module.blog_sg.security_group_id]
+      
+      ami = data.aws_ami.app_ami.id
+      instance_type = var.instance_type
+
+        
+  # insert the 1 required variable here
 }
     
-    module "alb" {
+    module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
